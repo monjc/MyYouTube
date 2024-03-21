8d7fad0541c06b29fb5c1f8918c5cee9ee58a5ba
Mojo Dojo Code House <>
2024-03-20 18:01:17 -0600

    Clase 2 Integración de Assets y configuración Inicial del LaunchScreen

---
+++ MyYouTube/MyYouTube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyYouTube
+//
+//  Created by Mojo Dojo Code House on 20/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
